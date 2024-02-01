e5ab25057b01a93e718de64984bf3497f7f48229
ysm10801 <ysm10801@gmail.com>
2024-02-01 19:18:09 +0900

    commit

--- build/catkin_generated/order_packages.py
+++ build/catkin_generated/order_packages.py
@@ -2,4 +2,4 @@
 source_root_dir = '/home/yang/catkin_ws/src'
 whitelisted_packages = ''.split(';') if '' != '' else []
 blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/opt/ros/noetic'.split(';') if '/opt/ros/noetic' != '' else []
+underlay_workspaces = '/home/yang/catkin_ws/devel;/opt/ros/noetic'.split(';') if '/home/yang/catkin_ws/devel;/opt/ros/noetic' != '' else []
